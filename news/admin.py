0a8572db72c851d5c444931c08e13e68b9e0dc5a
Kamo Petrosyan <kamo@haikson.com>
2014-03-23 21:54:06 +0700

    Update admin.py

--- news/admin.py
+++ news/admin.py
@@ -1,17 +1,16 @@
 from django.contrib import admin
 from models import News
 from django import forms
-
+from django.conf import settings
 class NewsAdmin(admin.ModelAdmin):
     list_display = ('title', 'created', 'published')
 
     class Media:
-        js = (
-            #'/media/js/ckeditor/ckeditor.js',
-            #'/media/js/ckeditor_filer.js',
-            #'/media/filebrowser/js/fb_ckeditor.js',
-            '/static/filebrowser/js/FB_CKEditor.js',
-        )
-        #css = { 'all': ('/media/js/ckeditor/css/ckeditor.css',),}
-
+        if 'filebrowser' in settings.INSTALLED_APP:
+            js = (
+                '/static/filebrowser/js/FB_CKEditor.js',
+            )
+        else:
+            pass
+        
 admin.site.register(News, NewsAdmin)
